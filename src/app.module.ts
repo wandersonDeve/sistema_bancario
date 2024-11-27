ca87b18b9f1497f272b201a320ab3a859e89d2a3
Wanderson Santos <81826043+wandersonDeve@users.noreply.github.com>
2024-11-27 18:54:33 -0300

    chore(throttler): add lib throttler and config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
-  imports: [],
+  imports: [
+    ThrottlerModule.forRoot([
+      {
+        ttl: +process.env.THROTTLE_TTL,
+        limit: +process.env.THROTTLE_LIMIT,
+      },
+    ]),
+  ],
   controllers: [],
   providers: [],
 })
